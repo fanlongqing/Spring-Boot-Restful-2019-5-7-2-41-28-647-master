c7181e8d570ffe81c339619a7f7f00ae5b0c4f87
fan <610398596@qq.com>
2019-08-09 11:39:44 +0800

    Initial commit

---
+++ src/main/java/com/example/SpringBootRestful/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.example.SpringBootRestful.controller;
+
+import com.example.SpringBootRestful.entity.User;
+import com.example.SpringBootRestful.exception.BusinessException;
+import com.example.SpringBootRestful.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("")
+//ToDo:在上一行代码("")中填写合适的RestfulApi
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/getAllUser")
+    //ToDo:在上一行代码("")中填写合适的RestfulApi
+    public ResponseEntity getAllUser() {
+        return new ResponseEntity<>(userService.getAllUser(), HttpStatus.OK);
+    }
+
+    @PostMapping("/addUser")
+    //ToDo:在上一行代码("")中填写合适的RestfulApi
+    public ResponseEntity addUser(@RequestBody User user) {
+        userService.addUser(user);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/updateUserById/{userId}")
+    //ToDo:在上一行代码("")中填写合适的RestfulApi
+    public ResponseEntity updateUserById(@PathVariable int userId, @RequestBody User user) throws BusinessException {
+        userService.updateUserById(userId, user);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/deleteUserById/{userId}")
+    //ToDo:在上一行代码("")中填写合适的RestfulApi
+    public ResponseEntity deleteUserById(@PathVariable int userId) throws BusinessException {
+        userService.deleteUserById(userId);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
